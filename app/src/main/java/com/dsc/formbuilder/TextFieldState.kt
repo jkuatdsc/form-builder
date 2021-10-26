6737414226feddc66dfec8e7a064c98d5aeccfc5
Otieno Samwel <otienosamwel135@gmail.com>
2021-10-26 22:54:58 +0300

    - create function implementations for validators

--- app/src/main/java/com/dsc/formbuilder/TextFieldState.kt
+++ app/src/main/java/com/dsc/formbuilder/TextFieldState.kt
@@ -2,5 +2,23 @@ package com.dsc.formbuilder
 
 class TextFieldState {
 
-    // add the validators implementation here...
+    fun requiredValidator(input: String): Boolean {
+        return input.isEmpty()
+    }
+
+    fun emailValidator(regexPattern: Regex, email: String): Boolean {
+        return regexPattern.containsMatchIn(email)
+    }
+
+    fun phoneNumberValidator(regexPattern: Regex, phoneNumber: Long): Boolean {
+        return regexPattern.containsMatchIn(phoneNumber.toString())
+    }
+
+    fun minValidator(limit: Double, input: String): Boolean {
+        return input.toDouble() > limit
+    }
+
+    fun maxValidator(limit: Double, input: String): Boolean {
+        return input.toDouble() < limit
+    }
 }
