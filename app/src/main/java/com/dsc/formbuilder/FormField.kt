a017f345a3d6bb014c69fcb6ccede24a1169b686
moose <linus.m.muema@gmail.com>
2021-10-29 11:00:50 +0300

    - added basic form functions

---
+++ app/src/main/java/com/dsc/formbuilder/FormField.kt
@@ -0,0 +1,71 @@
+package com.dsc.formbuilder
+
+import android.util.Patterns
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class FormField(val name: String, initial: String = "", val validators: List<Validators>) {
+
+    var text: String by mutableStateOf(initial)
+    var message: String by mutableStateOf("")
+    var hasError: Boolean by mutableStateOf(false)
+
+    @Composable
+    internal fun Content(){
+        TextField(value = text, onValueChange = { text = it })
+    }
+
+    fun showError(error: String){
+        hasError = true
+        message = error
+    }
+
+    fun hideError(){
+        message = ""
+        hasError = false
+    }
+
+    fun clear() = "".also { text = it }
+
+    internal fun validate(): Boolean {
+        return validators.map {
+            when (it){
+                is Email -> {
+                    if (!email()) showError(it.message)
+                    email()
+                }
+                is PhoneNumber -> {
+                    if (!phone()) showError(it.message)
+                    phone()
+                }
+                is Required -> {
+                    if (!required()) showError(it.message)
+                    required()
+                }
+                is Max -> {
+                    if (!max(it.limit)) showError(it.message)
+                    max(it.limit)
+                }
+                is Min -> {
+                    if (!min(it.limit)) showError(it.message)
+                    min(it.limit)
+                }
+            }
+        }.all { it }
+    }
+
+
+    private fun required(): Boolean = text.isEmpty()
+
+    private fun max(limit: Double): Boolean = text.toDouble() < limit
+
+    private fun min(limit: Double): Boolean = text.toDouble() > limit
+
+    private fun email(): Boolean = Patterns.EMAIL_ADDRESS.matcher(text).matches()
+
+    private val phoneRegex = Regex("^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}\$")
+    private fun phone(): Boolean = phoneRegex.containsMatchIn(text)
+}
--- app/src/main/java/com/dsc/formbuilder/MainActivity.kt
+++ app/src/main/java/com/dsc/formbuilder/MainActivity.kt
@@ -1,13 +1,48 @@
 package com.dsc.formbuilder
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Content()
+        }
+    }
+
+    @Composable
+    fun Content() {
+        Column {
+            val state by remember { mutableStateOf(FormState()) }
+            Form(
+                state = state,
+                fields = listOf(
+                    FormField(
+                        name = "email",
+                        validators = listOf(Email())
+                    )
+                )
+            )
+            Button(
+                content = { Text("Click me") },
+                onClick = {
+                    if (state.validate()) {
+                        Log.d("Validators", "onCreate: We are good to go")
+                    } else {
+                        Log.e("Validators", "Content: We are not good to go")
+                    }
+                }
+            )
         }
     }
 }
