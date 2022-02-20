6728a718d523f3c71191848e2fcfe3cebd504c71
OtienoSamwel <otienosamwel135@gmail.com>
2022-02-20 21:16:30 +0300

    created more input field examples

--- example/src/main/java/com/dsc/formbuilder/MainActivity.kt
+++ example/src/main/java/com/dsc/formbuilder/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.dsc.formbuilder
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.dsc.form_builder.TextFieldState
 
 class MainActivity : ComponentActivity() {
     private val viewmodel: MainViewmodel by viewModels()
@@ -29,16 +29,22 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun Content() {
         val formState = remember { viewmodel.formState }
-
         val ageState = formState.getState("age")
         val emailState = formState.getState("email")
         val passwordState = formState.getState("password")
+        val genderState = formState.getState("gender")
+        val happinessState = formState.getState("happiness")
+        val hobbiesState = formState.getState("hobbies")
+        val scrollState = rememberScrollState()
 
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(state = scrollState),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+            ) {
 
 
             OutlinedTextField(
@@ -69,11 +75,93 @@ class MainActivity : ComponentActivity() {
             )
             if (ageState.hasError) Text(ageState.errorMessage, color = Color.Red)
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            SelectGender(genderState = genderState)
+
+            if (genderState.hasError) Text(genderState.errorMessage, color = Color.Red)
+
+            Spacer(modifier = Modifier.height(20.dp))
+            HappinessIndex(happinessState = happinessState)
+
+            if (happinessState.hasError) Text(happinessState.errorMessage, color = Color.Red)
+
+            Hobbies(hobbyState = hobbiesState)
+
+            if (hobbiesState.hasError) Text(hobbiesState.errorMessage, color = Color.Red)
+
             Button(
                 modifier = Modifier.padding(16.dp),
                 onClick = { viewmodel.submit() },
                 content = { Text("submit") },
             )
+        }
+    }
+
+    @Composable
+    fun SelectGender(genderState: TextFieldState<*>) {
+        val radioGroupOptions = listOf(
+            "Male",
+            "Female",
+            "Non Binary",
+            "Prefer not to say"
+        )
+        val selected = genderState.text
+        val onSelectedChanged = { text: String -> genderState.change(text) }
+        Column(
+            modifier = Modifier
+                .selectableGroup()
+        ) {
+            radioGroupOptions.forEach {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    RadioButton(
+                        selected = selected == it,
+                        onClick = { onSelectedChanged(it) },
+                    )
+                    Text(text = it, modifier = Modifier.padding(top = 14.dp))
+                }
+            }
+
+        }
+    }
+
+    @Composable
+    fun HappinessIndex(happinessState: TextFieldState<*>) {
+        val value = if (happinessState.text == "") 0.0f else happinessState.text.toFloat()
+        Text(text = "Happiness level: ${(value * 100).toInt()}")
+        Slider(
+            value = value,
+            onValueChange = { happinessState.change(it.toString()) })
+    }
+
+    @Composable
+    fun Hobbies(hobbyState: TextFieldState<*>) {
+        val hobbiesList = listOf("Chess", "Sky Diving", "Reading", "Travelling")
+        val selectedHobbies = remember { hobbyState.text.split(",").toMutableList() }
+
+        fun onHobbySelected(add: Boolean, text: String) {
+            if (add) {
+                selectedHobbies.add(text)
+                hobbyState.change(selectedHobbies.toString())
+            } else if (!add) {
+                selectedHobbies.remove(text)
+                hobbyState.change(selectedHobbies.toString())
+            }
+        }
+
+        Text(text = "Hobbies")
+
+        hobbiesList.forEach { hobby ->
+            var isChecked by remember { mutableStateOf(false) }
+            Row(Modifier.fillMaxWidth()) {
+                Checkbox(
+                    checked = isChecked,
+                    onCheckedChange = {
+                        isChecked = it
+                        onHobbySelected(it, text = hobby)
+                    })
+                Text(text = hobby, modifier = Modifier.padding(top = 14.dp))
+            }
         }
     }
 }
--- example/src/main/java/com/dsc/formbuilder/MainViewmodel.kt
+++ example/src/main/java/com/dsc/formbuilder/MainViewmodel.kt
@@ -1,8 +1,6 @@
 package com.dsc.formbuilder
 
 import android.util.Log
-import android.widget.Toast
-import androidx.compose.ui.text.toLowerCase
 import androidx.lifecycle.ViewModel
 import com.dsc.form_builder.FormState
 import com.dsc.form_builder.TextFieldState
@@ -26,11 +24,27 @@ class MainViewmodel : ViewModel() {
                 transform = { it.toInt() },
                 validators = listOf(Validators.MinValue(limit = 18, message = "too young"))
             ),
+
+            TextFieldState(
+                name = "gender",
+                validators = listOf(Validators.Required(message = "you need to specify your gender"))
+            ),
+            TextFieldState(
+                name = "happiness",
+                transform = { it.toFloat() },
+                validators = listOf(Validators.Required(message = "how happy are you?"))
+            ),
+            TextFieldState(
+                name = "hobbies",
+                transform = { it.split(",").toList() },
+                validators = listOf(Validators.Required(message = "pick at least one hobby"))
+            )
+
         )
     )
 
-    fun submit(){
-        if (formState.validate()){
+    fun submit() {
+        if (formState.validate()) {
             val data = formState.getData(Credentials::class)
             Log.d("Data", "submit: data from the form $data")
         }
