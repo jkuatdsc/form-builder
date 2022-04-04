68b046fe599b95ed8076e2d415e017668a48c0d1
linusmuema <linus.m.muema@gmail.com>
2022-04-04 21:04:22 +0300

    - added dokker

--- form-builder/src/main/java/com/dsc/form_builder/BaseState.kt
+++ form-builder/src/main/java/com/dsc/form_builder/BaseState.kt
@@ -4,6 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
+/**
+ * This is the abstract base state that defines the basic behaviour for all form states.
+ *
+ * @param name this is the name of the state. It is used to access the state when required in the form. It is also used when creating the class in the [getData] method of the [FormState] class.
+ * @param transform this function is used to change the data type in the field state. You can use it to convert the data in the field to your preferred type e.g [String] to [Int]
+ */
 abstract class BaseState<T>(
     val name: String,
     val transform: Transform<T>?,
