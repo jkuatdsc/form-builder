2a42b562f011a252a8b366e39129d5669acf583e
joykangangi <64706463+joykangangi@users.noreply.github.com>
2022-04-06 14:51:43 +0300

    Transform Docs

--- form-builder/src/main/java/com/dsc/form_builder/Transform.kt
+++ form-builder/src/main/java/com/dsc/form_builder/Transform.kt
@@ -1,5 +1,17 @@
 package com.dsc.form_builder
 
+/**
+ * This interface is used to allow change of data types to a suitable type when necessary.
+ *
+ *@author [Joy Kangangi](https://github.com/joykangangi)
+ * @created 06/04/2022 - 2:35 p.m.
+ */
+
 fun interface Transform<T> {
+    /**
+     * this abstract method is used to change the [value] data type to a desired type and return [Any]?.
+     * @param value the value of type [T] to be parsed to [Any].
+     * For example to read 'age' value from a field [TextFieldState], it will be transformed from [String] to [Int].
+     */
     fun transform(value: T): Any?
 }
