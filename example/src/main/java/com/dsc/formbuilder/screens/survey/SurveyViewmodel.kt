4fed0937d837a0a6834018fa75e6412eac08dfc6
linusmuema <linus.m.muema@gmail.com>
2022-08-22 16:25:22 +0200

    - added the navigation variable

--- example/src/main/java/com/dsc/formbuilder/screens/survey/SurveyViewmodel.kt
+++ example/src/main/java/com/dsc/formbuilder/screens/survey/SurveyViewmodel.kt
@@ -1,5 +1,16 @@
 package com.dsc.formbuilder.screens.survey
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class SurveyViewmodel: ViewModel()
+class SurveyViewmodel: ViewModel(){
+
+    private val _screen: MutableState<Int> = mutableStateOf(0)
+    val screen: State<Int> = _screen
+
+    fun navigate(screen: Int){
+        _screen.value = screen
+    }
+}
