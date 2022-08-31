48ed5aed69631536c4ff162959f94bf9f9ba0cde
joykangangi <64706463+joykangangi@users.noreply.github.com>
2022-08-31 14:01:51 +0300

    Feature Intro&Exit Screens

--- example/src/main/java/com/dsc/formbuilder/screens/ExitActivity.kt
+++ example/src/main/java/com/dsc/formbuilder/screens/ExitActivity.kt
@@ -1,33 +1,52 @@
 package com.dsc.formbuilder.screens
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.dsc.formbuilder.R
 import com.dsc.formbuilder.theme.FormBuilderTheme
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
 class ExitActivity : ComponentActivity() {
-    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val finishActivity: () -> Unit = {
+            this.finish()
+            Log.d("ExitActivity", "Finish Called")
+        }
         setContent {
             FormBuilderTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    ExitScreen(animatedNavController = rememberAnimatedNavController())
+                val screenState = remember {
+                    MutableTransitionState(false).apply {
+                        targetState = true
+                    }
+                }
+                AnimatedVisibility(
+                    visibleState = screenState,
+                    enter = slideInHorizontally(),
+                    exit = slideOutHorizontally()
+                ) {
+                    Surface(color = MaterialTheme.colors.background) {
+                        ExitScreen(activityFinish = finishActivity)
+                    }
                 }
             }
         }
@@ -35,10 +54,32 @@ class ExitActivity : ComponentActivity() {
 }
 
 @Composable
-fun ExitScreen(modifier: Modifier = Modifier, animatedNavController: NavController) {
+fun ExitScreen(modifier: Modifier = Modifier, activityFinish:()->Unit) {
     val black = MaterialTheme.colors.onPrimary
     val white = MaterialTheme.colors.background
     val textStyle = MaterialTheme.typography.body1
+    val context = LocalContext.current
+    val intent = Intent(context, IntroActivity::class.java)
+    val infiniteTransition = rememberInfiniteTransition()
+
+    val imageAnim by infiniteTransition.animateFloat(
+        initialValue = 210f,
+        targetValue = 240f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val angleOffset = 15f
+    val angleAnimation by infiniteTransition.animateFloat(
+        initialValue = -angleOffset,
+        targetValue = angleOffset ,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 1000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
 
     Column(
         modifier
@@ -47,14 +88,20 @@ fun ExitScreen(modifier: Modifier = Modifier, animatedNavController: NavControll
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.like),
-            contentDescription = "Thumbs Up"
-        )
+        Box(
+            modifier = modifier.size(300.dp),
+            contentAlignment = Alignment.Center) {
+            Image(
+                painter = painterResource(id = R.drawable.like),
+                contentDescription = "Thumbs Up",
+                modifier = modifier.size(imageAnim.dp).rotate(angleAnimation)
+            )
+        }
         Text(text = stringResource(id = R.string.thanks), style = textStyle)
         Button(
             onClick = {
-                animatedNavController.navigate("intro")
+                context.startActivity(intent)
+                activityFinish()
             },
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = black,
@@ -68,13 +115,12 @@ fun ExitScreen(modifier: Modifier = Modifier, animatedNavController: NavControll
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Preview
 @Composable
 fun ExitPreview() {
     FormBuilderTheme {
         Surface(color = MaterialTheme.colors.background) {
-            ExitScreen(animatedNavController = rememberAnimatedNavController())
+            ExitScreen(activityFinish = { })
         }
     }
 }
--- example/src/main/java/com/dsc/formbuilder/screens/IntroActivity.kt
+++ example/src/main/java/com/dsc/formbuilder/screens/IntroActivity.kt
@@ -1,37 +1,38 @@
 package com.dsc.formbuilder.screens
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.*
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dsc.formbuilder.R
-import com.dsc.formbuilder.animations.PulsingButton
+import com.dsc.formbuilder.screens.survey.SurveyActivity
 import com.dsc.formbuilder.theme.FormBuilderTheme
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
-import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
 class IntroActivity : ComponentActivity() {
 
@@ -39,59 +40,32 @@ class IntroActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FormBuilderTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    AppNavigations()
+                val screenState = remember {
+                    MutableTransitionState(false).apply {
+                        targetState = true
+                    }
+                }
+                AnimatedVisibility(
+                    visibleState = screenState,
+                    enter = slideInHorizontally(),
+                    exit = slideOutHorizontally()
+                ) {
+                    Surface(color = MaterialTheme.colors.background) {
+                        IntroScreen()
+                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun AppNavigations() {
-    val navController = rememberAnimatedNavController()
-    AnimatedNavHost(navController = navController, startDestination = "intro") {
-        composable(
-            "intro",
-            enterTransition = {
-                slideInHorizontally(initialOffsetX = { 1000 })
-            },
-            exitTransition = {
-                slideOutHorizontally(targetOffsetX = { -1000 })
-            },
-            popEnterTransition = {
-                slideInHorizontally(initialOffsetX = { -1000 })
-            },
-            popExitTransition = {
-                slideOutHorizontally(targetOffsetX = { 1000 })
-            }
-        ) { IntroScreen(
-            onStartClick = { navController.navigate("form_inputs") }) }
+fun IntroScreen(modifier: Modifier = Modifier) {
+    val black = MaterialTheme.colors.onPrimary
+    val white = MaterialTheme.colors.background
+    val context = LocalContext.current
+    val intent = Intent(context, SurveyActivity::class.java)
 
-        composable(
-            "exit",
-            enterTransition = {
-                slideInHorizontally(initialOffsetX = { 1000 })
-            },
-            exitTransition = {
-                slideOutHorizontally(targetOffsetX = { -1000 })
-            },
-            popEnterTransition = {
-                slideInHorizontally(initialOffsetX = { -1000 })
-            },
-            popExitTransition = {
-                slideOutHorizontally(targetOffsetX = { 1000 })
-            }
-        ) {
-            ExitScreen(animatedNavController = navController)
-        }
-    }
-}
-
-
-@Composable
-fun IntroScreen(modifier: Modifier = Modifier, onStartClick: ()->Unit) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -102,17 +76,64 @@ fun IntroScreen(modifier: Modifier = Modifier, onStartClick: ()->Unit) {
         Spacer(modifier = modifier.height(150.dp))
 
         Box(
-            modifier.size(300.dp),
+            modifier
+                .size(300.dp),
             contentAlignment = Alignment.Center
         ) {
             //this box prevents the animation from mis-positioning the other elements in the column
-            PulsingButton(onStartClicked = onStartClick)
+
+            val infiniteTransition = rememberInfiniteTransition()
+
+            val pulseMagnitude by infiniteTransition.animateFloat(
+                initialValue = 240f,
+                targetValue = 270f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+
+            val textSizeInfinite = infiniteTransition.animateFloat(
+                initialValue = MaterialTheme.typography.h4.fontSize.value,
+                targetValue = MaterialTheme.typography.h3.fontSize.value,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(1000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+
+            // The start Button
+            Card(
+                modifier
+                    .height(pulseMagnitude.dp)
+                    .width(pulseMagnitude.dp)
+                    .padding(all = 12.dp)
+                    .background(color = white)
+                    .clickable(onClick = {
+                        context.startActivity(intent)
+                    }),
+                elevation = 5.dp,
+                shape = CircleShape
+            ) {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.start),
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily.SansSerif,
+                        fontSize = textSizeInfinite.value.sp
+                    )
+                }
+            }
         }
         Box(
             modifier = modifier
                 .size(300.dp),
             contentAlignment = Alignment.BottomCenter
         ) {
+            var switchOn by remember { mutableStateOf(true) }
             Row(
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
@@ -123,66 +144,17 @@ fun IntroScreen(modifier: Modifier = Modifier, onStartClick: ()->Unit) {
                 )
                 Spacer(modifier = modifier.width(24.dp))
 
-                CustomSwitch()
+                Switch(
+                    modifier = modifier.semantics { contentDescription = "Switch" },
+                    checked = switchOn,
+                    onCheckedChange = { switchOn = it },
+                    colors = SwitchDefaults.colors(
+                        checkedTrackColor = black
+                    )
+                )
             }
         }
     }
-}
-
-@Composable
-fun CustomSwitch(
-    modifier: Modifier = Modifier,
-    width: Dp = 60.dp,
-    height: Dp = 25.dp,
-    thumbColor: Color = MaterialTheme.colors.background,
-    checkedTrackColor: Color = MaterialTheme.colors.onPrimary,
-    uncheckedTrackColor: Color = MaterialTheme.colors.onSurface,
-    spaceBetweenThumbAndTrackEdge: Dp = 1.dp,
-    cornerSize: Int = 60,
-    thumbSize: Dp = 45.dp
-) {
-    //disable ripple effect on custom switch
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    //state of the switch
-    var switchOn by remember { mutableStateOf(true) }
-
-    //move thumb horizontally using bias +ve f->end, -ve f->start
-    val alignment by animateAlignmentAsState(if (switchOn) 2f else -2f)
-
-    //outer rectangle->track
-    Box(
-        modifier = modifier
-            .size(width = width, height = height)
-            .clip(RoundedCornerShape(cornerSize))
-            .background(color = if (switchOn) checkedTrackColor else uncheckedTrackColor)
-            .clickable(
-                indication = null,
-                interactionSource = interactionSource
-            ) { switchOn = !switchOn },
-        contentAlignment = alignment
-    ) {
-        //thumb
-        Canvas(modifier = modifier.size(thumbSize)) {
-            val thumbRadius = (height / 2) - spaceBetweenThumbAndTrackEdge
-            drawCircle(
-                color = thumbColor,
-                radius = thumbRadius.toPx()
-            )
-        }
-    }
-}
-
-//Todo: implement validate on screen button
-
-@Composable
-private fun animateAlignmentAsState(
-    targetBiasValue: Float
-): State<BiasAlignment> {
-    val bias by animateFloatAsState(targetValue = targetBiasValue)
-    return derivedStateOf { BiasAlignment(horizontalBias = bias, verticalBias = 0f) }
-
 }
 
 @Preview
@@ -190,7 +162,7 @@ private fun animateAlignmentAsState(
 fun IntroPreview() {
     FormBuilderTheme {
         Surface(color = MaterialTheme.colors.background) {
-           IntroScreen(onStartClick = { })
+            IntroScreen()
         }
     }
 }
