a6776cdab2320e96e7487b2fe826d9f8b8d79f29
linusmuema <linus.m.muema@gmail.com>
2022-09-14 13:24:46 +0200

    - finish connecting the navigation

--- example/src/main/java/com/dsc/formbuilder/screens/survey/SurveyActivity.kt
+++ example/src/main/java/com/dsc/formbuilder/screens/survey/SurveyActivity.kt
@@ -3,13 +3,50 @@ package com.dsc.formbuilder.screens.survey
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.dsc.form_builder.FormState
+import com.dsc.formbuilder.screens.survey.components.*
 import com.dsc.formbuilder.theme.FormBuilderTheme
 
 class SurveyActivity : ComponentActivity() {
+    private val viewmodel: SurveyViewmodel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             FormBuilderTheme {
+                val screen by remember { viewmodel.screen }
+                val validate = intent.getBooleanExtra("validate", true)
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    TabLayout(screen = screen)
+
+//                    when(screen){
+//                        0 -> PersonalDetails(formState = formstate)
+//                        1 -> TechnicalDetails(formState = formstate)
+//                        3 -> OtherDetails(formState = formstate)
+//                    }
+
+                    Navigation(screen = screen) {
+                        if (validate) {
+                            viewmodel.validateScreen(screen)
+                        } else {
+                            if (screen == 2) viewmodel.validateSurvey()
+                            else viewmodel.navigate(screen = screen + 1)
+                        }
+                    }
+                }
 
             }
         }
