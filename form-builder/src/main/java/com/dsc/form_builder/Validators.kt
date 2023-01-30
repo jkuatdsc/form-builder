81f35144d1ef4179215791387d5bb49d3f7520fb
joykangangi <64706463+joykangangi@users.noreply.github.com>
2023-01-30 10:03:03 +0300

    custom validator example

--- form-builder/src/main/java/com/dsc/form_builder/Validators.kt
+++ form-builder/src/main/java/com/dsc/form_builder/Validators.kt
@@ -76,6 +76,12 @@ sealed interface Validators {
 
     /**
      * This is validator gives you the option to provide your own implementation of the validator. You can pass in a custom function to validate the field value.
+     *
+     * Example:
+     *
+     *[Validators].[Custom] ([message] = "A custom message" , [function] = { customFunc(customParam) } )
+     *
+     *
      * @param function this is the lambda function that is called during validation. It provides the field value as a parameter and expects a [Boolean] return value.
      * @param message the error message to display if the validation fails.
      */
