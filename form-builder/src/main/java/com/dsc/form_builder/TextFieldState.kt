ef99b80003daa229d712028f03af751c0fa3cae5
linusmuema <linus.m.muema@gmail.com>
2023-05-04 08:23:08 +0200

    - added the luhn algorithm implementation

--- form-builder/src/main/java/com/dsc/form_builder/TextFieldState.kt
+++ form-builder/src/main/java/com/dsc/form_builder/TextFieldState.kt
@@ -120,28 +120,22 @@ open class TextFieldState(
     }
 
     /**
-     *This function validates a Card Number in [value]
-     *It will return true if the string value is a valid card number.
-     *@param message the error message passed to [showError] to display if the value is not a valid card number. By default we use the [CARD_NUMBER_MESSAGE] constant.
+     * This function validates a Card Number in [value]
+     * It will return true if the string value is a valid card number. This function makes use of the [Luhn Algorithm](https://www.creditcardvalidator.org/articles/luhn-algorithm) to verify the validity of the credit cards.
+     * @param message the error message passed to [showError] to display if the value is not a valid card number. By default we use the [CARD_NUMBER_MESSAGE] constant.
      */
     internal fun validateCardNumber(message: String): Boolean {
-        val cardNumberRegex = "(^3[47][0-9]{13}\$)|" +
-                "(^(6541|6556)[0-9]{12}\$)|" +
-                "(^389[0-9]{11}\$)|" +
-                "(^3(?:0[0-5]|[68][0-9])[0-9]{11}\$)|" +
-                "(^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})\$\n)|" +
-                "(^63[7-9][0-9]{13}\$)|" +
-                "(^(?:2131|1800|35\\d{3})\\d{11}\$)|" +
-                "(^9[0-9]{15}\$)|" +
-                "(^(6304|6706|6709|6771)[0-9]{12,15}\$)|" +
-                "(^(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}\$)|" +
-                "(^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))\$\n)|" +
-                "(^(6334|6767)[0-9]{12}|(6334|6767)[0-9]{14}|(6334|6767)[0-9]{15}\$)|" +
-                "(^(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}\$\n)|" +
-                "(^(62[0-9]{14,17})\$)|" +
-                "(^4[0-9]{12}(?:[0-9]{3})?\$)|" +
-                "(^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})\$)"
-        val valid = cardNumberRegex.toRegex().matches(value)
+        var checksum = 0
+
+        for (i in value.length - 1 downTo 0 step 2) {
+            checksum += value[i] - '0'
+        }
+        for (i in value.length - 2 downTo 0 step 2) {
+            val n: Int = (value[i] - '0') * 2
+            checksum += if (n > 9) n - 9 else n
+        }
+
+        val valid = checksum%10 == 0
         if (!valid) showError(message)
         return valid
     }
